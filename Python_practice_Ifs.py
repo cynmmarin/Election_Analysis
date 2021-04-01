27b1b14c7355096399bf425500f6dcf9a2cafbe6
Cynthia <cynthiammarin@gmail.com>
2021-04-01 15:04:34 -0700

    Adding PyPoll.py file

---
+++ Python_practice_Ifs.py
@@ -0,0 +1,29 @@
+#What is the score?
+score = int(input("What is your test score? "))
+
+# Determine the grade.
+if score >= 90:
+    print('Your grade is an A.')
+elif score >= 80:
+    print('Your grade is a B.')
+elif score >= 70:
+    print('Your grade is a C.')
+elif score >= 60:
+    print('Your grade is a D.')
+else:
+    print('Your grade is an F.')
+
+x = 0
+while x <=5
+    print(x)
+    x = x + 1
+
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+for county in counties_dict:
+    print(county)
+
+for county in counties_dict.keys():
+    print(county)
+    
+for voters in counties_dict.values():
+    print(voters)  
